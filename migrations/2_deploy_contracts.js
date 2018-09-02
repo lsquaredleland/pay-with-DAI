4a85589ece29c00dce2f98fd7943608785d3b844
Leland Lee <lelandlee4@gmail.com>
2018-09-02 01:17:59 -0700

    reorg

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,14 +9,14 @@ module.exports = function(deployer) {
 	deployer
 		.deploy(TestToken)
 		.then(() => {
-		  return DelegateBank.new(TestToken.address);
+		  return deployer.deploy(DelegateBank, TestToken.address);
 		})
 		.then((instance) => {
 			DelegateBankInstance = instance;
-		  return wToken.new(TestToken.address, DelegateBank.address);
+		  return deployer.deploy(wToken, TestToken.address, DelegateBank.address);
 		})
 		.then(() => {
-			return PayWithToken.new(TestToken.address, wToken.address, DelegateBank.address);
+			return deployer.deploy(PayWithToken, TestToken.address, wToken.address, DelegateBank.address);
 		})
 		.then((instance) => {
 			PayWithTokenInstance = instance;
